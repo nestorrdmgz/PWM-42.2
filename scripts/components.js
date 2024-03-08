bf6e897549e9d01c2eaac76e2e3ea208c53824b2
imano <imanolquinber26@gmail.com>
2024-03-08 17:26:11 +0000

    Página para añadir partidos hecha. Modificado el Register Page y el Login Page.

--- scripts/components.js
+++ scripts/components.js
@@ -1,4 +1,4 @@
-// Función para cargar el header
+// Función para cargar el botón de añadir partido
 function loadAddMatch() {
     fetch('components/add_match_button.html')
         .then(response => response.text())
@@ -7,7 +7,7 @@ function loadAddMatch() {
         })
         .catch(error => console.error('Error cargando el add match:', error));
 }
-// Función para cargar el footer
+// Función para cargar el botón de filtro
 function loadFilterButton() {
     fetch('components/filter_button.html')
         .then(response => response.text())
@@ -15,4 +15,16 @@ function loadFilterButton() {
             document.getElementById('filter_button').innerHTML = html;
         })
         .catch(error => console.error('Error cargando el filter button:', error));
+}
+// Función para cargar el detalle de los partidos
+function loadMatchDetails() {
+    fetch('components/match_details.html')
+        .then(response => response.text())
+        .then(html => {
+            const elements = document.querySelectorAll('.match_details');
+            elements.forEach(element => {
+                element.innerHTML = html;
+            });
+        })
+        .catch(error => console.error('Error cargando el detalle de los partidos:', error));
 }
