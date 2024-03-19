ac6dbcf1e1e51ef2e0f7a40790aca3f6795de869
AlvaroRodguez <alvarorodriguezteimoy@gmail.com>
2024-03-19 13:38:13 +0000

    loadTemplate function created, removed other load functions

--- scripts/header_footer.js
+++ scripts/header_footer.js
@@ -1,56 +1,10 @@
 
 // Función para cargar el header
-function loadHeader() {
-    fetch('../templates/header.html')
+function loadTemplate(file, id) {
+    fetch(file)
         .then(response => response.text())
         .then(html => {
-            document.getElementById('header-container').innerHTML = html;
+            document.getElementById(id).innerHTML = html;
         })
-        .catch(error => console.error('Error cargando el header:', error));
+        .catch(error => console.error('Error loading template:', error));
 }
-// Función para cargar el header
-function loadLoggedHeader() {
-    fetch('../templates/header_with_login.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('header-container').innerHTML = html;
-        })
-        .catch(error => console.error('Error cargando el header:', error));
-}
-// Función para cargar el footer
-function loadFooter() {
-    fetch('../templates/footer.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('footer-container').innerHTML = html;
-        })
-        .catch(error => console.error('Error cargando el footer:', error));
-}
-
-function loadImage_profile() {
-    fetch('../components/image_profile.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('image-container').innerHTML = html;
-        })
-        .catch(error => console.error('Error cargando el container:', error));
-}
-
-function loadInfo_account() {
-    fetch('../components/info_account.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('info_account-container').innerHTML = html;
-        })
-        .catch(error => console.error('Error cargando el container:', error));
-}
-
-function loadPersonal_info() {
-    fetch('../components/personal_info.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('personal_info-container').innerHTML = html;
-        })
-        .catch(error => console.error('Error cargando el container:', error));
-}
-
